a5cef9ad3ed530135522c0da94ac91ad9eba0721
Soupayan <soupayan.thakur@gmail.com>
2020-12-20 23:05:57 +0530

    updated rust doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![doc(html_logo_url = "https://d30y9cdsu7xlg0.cloudfront.net/png/411962-200.png")]
 #![doc(html_playground_url = "https://play.rust-lang.org/")]
+
 //! This is a logic gates simulation crate built to demonstrate writing unit tests
-//! and integration tests
+//! and integration tests [![Build Status](https://www.travis-ci.com/Soupayan/logic_gates.svg?branch=master)](https://www.travis-ci.com/Soupayan/logic_gates)
 //! ```
 //! use logic_gates::and;
 //! assert_eq!(1, and(1,1))
